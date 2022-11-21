474e1817aa18fa789348913b08a9c510e5d0411d
Ci3t <ranigfx@gmail.com>
2022-11-21 20:20:31 +0200

    react

--- src/GameContext.jsx/GlobalGameContext.js
+++ src/GameContext.jsx/GlobalGameContext.js
@@ -13,7 +13,7 @@ function GlobalGameContext({children}) {
             try{
             const {data} = await axios.get('https://6373a9b9348e94729912f2b1.mockapi.io/crudMock/cardGame')
                 console.dir(data);
-                setCardsData(data)
+                setCardsData(randomazier(data))
             }catch(error){
                 console.log(error);
                 setError(error)
@@ -21,6 +21,24 @@ function GlobalGameContext({children}) {
         }
         getCards()
     },[])
+
+    function randomMath(num){
+        return Math.floor(Math.random()* num)
+    }
+
+ const randomazier = (array)=>{
+    let arr = [...array]
+        for (let i = 0; i < array.length; i++) {
+            arr.push(array[i])
+            
+        }
+       
+         return  arr.map((el,index,array)=>{
+            return array[randomMath(arr.length)]
+          } )
+        
+   }
+
 
 
   return (
