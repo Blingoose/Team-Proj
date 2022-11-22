dcffb58762487d6e5cc557525ca897c58f02c1f4
Andre Katov <andy.katov@icloud.com>
2022-11-22 09:22:27 +0200

    minor change

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,16 +1,17 @@
 import "./card.css";
 import "./flip-transition.css";
 
-function Card({ onClick, flip,image }) {
-
+function Card({ onClick, flip, image }) {
   const cardstyle = {
-    color:'red',
-    background: `url(${flip?image:'./Images/Uchiha.jpg'}) center center/cover no-repeat`
-  }
+    color: "red",
+    background: `url(${
+      flip ? image : "./Images/Uchiha.jpg"
+    }) center center/cover no-repeat`,
+  };
   return (
     <div className="card" onClick={onClick}>
-      <div style={cardstyle} className="card-back">Back</div>
-      <div style={cardstyle} className="card-front">Front</div>
+      <div style={cardstyle} className="card-back"></div>
+      <div style={cardstyle} className="card-front"></div>
     </div>
   );
 }
